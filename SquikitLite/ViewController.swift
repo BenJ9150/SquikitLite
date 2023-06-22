5f964e128aabcede592c5bcbfbdfc80b2661444c
benjamin <benj9150@gmail.com>
2023-06-22 19:13:09 +0200

    Initial Commit

---
+++ SquikitLite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquikitLite
+//
+//  Created by Benjamin on 22/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
